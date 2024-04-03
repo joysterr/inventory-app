aee893ed5ae2d00bfea6c6890af865d80932014b
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2024-04-03 20:19:17 +0100

    add individual item route, controller and view and fix css link
    updated css link in all views to link to static served file
    added individual item view
    added relevant route and controller code

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -10,8 +10,17 @@ exports.items_list_get = handler(async (req, res, next) => {
         .populate("category")
         .exec()
 
-    res.render('menu', { 
+    res.render('menu', {
         title: "Today's Menu",
-        items: allItems 
+        items: allItems
     })
+})
+
+// GET an individual item
+exports.item_get = handler(async (req, res, next) => {
+    const item = await Item.findById(req.params.id)
+        .populate("category")
+        .exec()
+
+    res.render('item', { item: item })
 })
--- routes/itemsRoute.js
+++ routes/itemsRoute.js
@@ -6,4 +6,7 @@ const Item = require('../controllers/itemsController')
 // GET items list
 router.get('/', Item.items_list_get)
 
+// GET item by id
+router.get('/:id', Item.item_get)
+
 module.exports = router
